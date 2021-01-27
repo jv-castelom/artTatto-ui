8aff005c64ca5002125eba07ce8549783228bdb1
jv-castelom <vitorcastelo.m3@gmail.com>
2021-01-26 21:27:38 -0300

    Create Photo Component

---
+++ src/app/components/photo/photo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'art-photo',
+  templateUrl: './photo.component.html',
+  styleUrls: ['./photo.component.css']
+})
+export class PhotoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
